ef8c61c283447992570622122e0747664da38c08
Danimlisto16 <50421224+Danimlisto16@users.noreply.github.com>
2021-01-02 13:16:18 -0500

    modelo cliente actualizado

--- Backend/backend_tienda_c2/src/main/java/com/tienda/backend/models/entities/Client.java
+++ Backend/backend_tienda_c2/src/main/java/com/tienda/backend/models/entities/Client.java
@@ -1,5 +1,152 @@
 package com.tienda.backend.models.entities;
 
-public class Client {
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+@Entity
+@Table(name = "Client")
+ 
+public class Client implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id  	//Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)   //Autoincrementable
+	@Column(name = "id_client")
+	@Basic (optional = false)
+	private long idclient;
+	
+	@Column(name = "document", length = 30)
+	private String document;
+	
+	@Column(name = "firstname", length = 30)
+	private String firstname;
+	
+	@Column(name = "lastname", length = 30)
+	private String lastname;
+	
+	@Column(name = "cellphone", length = 30)
+	private String cellphone;
+	
+	@Column(name = "email", length = 30)
+	private String email;
+	
+	@Column(name = "address", length = 30)
+	private String address;
+	
+	@Column(name = "borndate", length = 30)
+	private String borndate;
+	
+	@Column(name = "user", length = 30)
+	private String user;
+	
+	@Column(name = "password", length = 30)
+	private String password;
+	
+	
+	public Client() {
+		super();
+	}
+	
+	public Client(long id) {
+		super();
+		this.idclient = id;
+		
+	}
+
+	public long getIdclient() {
+		return idclient;
+	}
+
+	public void setIdclient(long idclient) {
+		this.idclient = idclient;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getCellphone() {
+		return cellphone;
+	}
+
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBorndate() {
+		return borndate;
+	}
+
+	public void setBorndate(String borndate) {
+		this.borndate = borndate;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+
+
 
 }
